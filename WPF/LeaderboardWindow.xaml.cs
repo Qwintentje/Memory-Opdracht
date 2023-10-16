0b081f77e4e313c118c06b2d726334ebe2d31bf2
Qwintentje <quintenvnimwegen@gmail.com>
2023-10-16 14:20:37 +0200

    Begin WPF

---
+++ WPF/LeaderboardWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+namespace WPF;
+public partial class LeaderboardWindow : Window
+{
+    private List<GameDbModel> leaderboard;
+    public LeaderboardWindow()
+    {
+        InitializeComponent();
+        Database db = new Database();
+        leaderboard = db.GetHighscores();
+        DataContext = leaderboard;
+    }
+
+    private void BackButton_Click(object sender, RoutedEventArgs e)
+    {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+        Close();
+    }
+}
--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,12 +1,23 @@
 ﻿namespace WPF;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
     }
+    private void PlayButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+
+    private void LeaderboardButton_Click(object sender, RoutedEventArgs e)
+    {
+        LeaderboardWindow leaderboardWindow = new LeaderboardWindow();
+        leaderboardWindow.Show();
+        Close();
+    }
+    private void QuitButton_Click(object sender, RoutedEventArgs e) => Close();
+
 }
